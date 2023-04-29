73fa7d57ce57b592c6ea58c8ec4c18c60b0c83b4
Messias Oliveira <messias@pentagro.com.br>
2023-04-29 08:13:28 -0300

    :alembic: "Finalizado implementação e testes"
    "Finalizado implementação e testes"

--- sketch.js
+++ sketch.js
@@ -1,13 +1,38 @@
+var train = true;
 
-function setup(){
-    createCanvas(500, 500);
-    background(0);
+function setup() {
+	createCanvas(500, 500);
+	background(0);
 
-    var rn = new RedeNeural(2, 3, 1);
-    var arr = [1, 2];
-    rn.feedForward(arr)
+	nn = new RedeNeural(2, 3, 1);
+
+	// XOR Problem
+	dataset = {
+		inputs:
+			[[1, 1],
+			[1, 0],
+			[0, 1],
+			[0, 0]],
+		outputs:
+			[[0],
+			[1],
+			[1],
+			[0]]
+	}
 }
 
-function draw(){
+function draw() {
+	if (train) {
+		for (var i = 0; i < 500; i++) {
+			var index = floor(random(4));
+			nn.train(dataset.inputs[index], dataset.outputs[index]);
+		}
+		console.log("🦾🤖 >> nn.predict([1, 0])[0]:", nn.predict([1, 0])[0])
+		console.log("🦾🤖 >> nn.predict([0, 0])[0]:", nn.predict([0, 0])[0])
 
+		if (nn.predict([0, 0])[0] < 0.04 && nn.predict([1, 0])[0] > 0.98) {
+			train = false;
+			console.log("terminou");
+		}
+	}
 }
